98dcd1c6ddaa9281593d159eda96796d810efb16
Jujhar Singh <jujhar@jujhar.com>
2021-02-20 22:05:41 +0000

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('satnaam');
+
+function ready (fn) {
+  if (document.readyState !== 'loading') {
+    render()
+  } else {
+    document.addEventListener('DOMContentLoaded', render)
+  }
+}
+
+const render = () => {
+  console.log('satnaam')
+}
+
+// kick off
+ready(() => { })
