f6707991f94868cea99ca26f6b61f74314d11fb3
Jujhar Singh <jujhar@jujhar.com>
2021-02-20 20:21:34 +0000

    fixes prod deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const environment = process.env?.NODE_ENV ? process.env?.NODE_ENV : 'development';
-
+let dest = 'docs';
+if (environment == "development") {
+  dest="build"
+}
 console.log(`Webpack running in ${environment}`);
 
 module.exports = {
@@ -11,7 +14,7 @@ module.exports = {
   mode: environment,
   output: {
     filename: 'js/main.js',
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, dest),
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -19,8 +22,8 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-          { from: 'src/css', to: path.resolve(__dirname, 'docs/css')},
-          { from: 'src/data', to: path.resolve(__dirname, 'docs/data')}
+          { from: 'src/css', to: path.resolve(__dirname, `${dest}/css`)},
+          { from: 'src/data', to: path.resolve(__dirname, `${dest}/data`)}
       ]
   })
   ]
